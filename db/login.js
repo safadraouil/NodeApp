fafcb06d16b1d108d0853fde94e00d69abd3981a
Draouil safa <draouilsafa@gmail.com>
2023-03-27 14:41:00 +0100

    token

--- db/login.js
+++ db/login.js
@@ -6,44 +6,47 @@ const dotenv = require("dotenv");
 dotenv.config(`${process.env.SECRET_KEY}`);
 
 exports.Login = async (req, res) => {
-  try {
-    const Login = req.body.Login;
-    const password = req.body.Password;
-    const UserType = req.body.UserType;
+  if (req.body.Password === undefined) {
+    return false;
+  } else
+    try {
+      const Login = req.body.Login;
+      const password = req.body.Password;
+      const UserType = req.body.UserType;
 
-    // Validate user input
-    if (!(Login && password)) {
-      res.status(400).send("All input is required");
-    }
-    // Validate if user exist in our database
-    const user = await userModel.UserModel.findOne(
-      { UserName: Login } || { UserLogin: Login }
-    );
-    if (
-      user &&
-      (await (password &&
-        user?.UserPassword &&
-        bcrypt.compare(password, user?.UserPassword)))
-    ) {
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, Login: Login, UserType },
-        "iamtryingtoaddsome",
-        {
-          expiresIn: "2h"
-        }
+      // Validate user input
+      if (!(Login && password)) {
+        res.status(400).send("All input is required");
+      }
+      // Validate if user exist in our database
+      const user = await userModel.UserModel.findOne(
+        { UserName: Login } || { UserLogin: Login }
       );
+      if (
+        user &&
+        (await (password &&
+          user?.UserPassword &&
+          bcrypt.compareSync(password, user?.UserPassword)))
+      ) {
+        // Create token
+        const token = jwt.sign(
+          { user_id: user._id, Login: Login, UserType },
+          "iamtryingtoaddsome",
+          {
+            expiresIn: "2h"
+          }
+        );
 
-      // save user token
-      user.token = token;
+        // save user token
+        user.token = token;
 
-      // user
-      res.status(200).json(user);
+        // user
+        res.status(200).json(user);
+      }
+      res.status(400).send("Invalid Credentials");
+    } catch (err) {
+      console.log(err);
     }
-    res.status(400).send("Invalid Credentials");
-  } catch (err) {
-    console.log(err);
-  }
 };
 
 exports.modules;
--- db/register.js
+++ db/register.js
@@ -6,55 +6,59 @@ const jwt = require("jsonwebtoken");
 exports.register = async (req, res) => {
   // Our register logic starts here
 
-  try {
-    // Get user input
+  if (req.body.password === undefined) return false;
+  else {
+    try {
+      // Get user input
 
-    const UserName = req.body.UserName;
-    const UserTel = req.body.Telephone;
-    const UserMail = req.body.Mail;
-    const UserLogin = req.body.Login;
-    const UserType = req.body.Type;
-    const UserPassword = req.body.password;
-    const UserCountry = req.body.country;
+      const UserName = req.body.UserName;
+      const UserTel = req.body.Telephone;
+      const UserMail = req.body.Mail;
+      const UserLogin = req.body.Login;
+      const UserType = req.body.Type;
+      const UserPassword = req.body.password;
+      const UserCountry = req.body.country;
 
-    // Validate user input
-    if (
-      !(
-        UserName &&
-        UserTel &&
-        UserMail &&
-        UserLogin &&
-        UserType &&
-        UserPassword &&
-        UserCountry
-      )
-    ) {
-      res.status(400).send("All input is required");
-    }
-    const oldUser = await userModel.UserModel.findOne({ UserMail });
-    if (oldUser) {
-      return res.status(409).send("User Already Exist. Please Login");
-    }
-    encryptedPassword = await (UserPassword && bcrypt.hash(UserPassword, 10));
-    const user = await userModel.UserModel.create({
-      UserName,
-      UserTel: parseInt(UserTel),
-      UserMail: UserMail.toLowerCase(),
-      UserPassword: encryptedPassword,
-      UserLogin,
-      UserType,
-      UserCountry
-    });
-    const token = jwt.sign(
-      { UserID: user._id, UserMail },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "2h"
+      // Validate user input
+      if (
+        !(
+          UserName &&
+          UserTel &&
+          UserMail &&
+          UserLogin &&
+          UserType &&
+          UserPassword &&
+          UserCountry
+        )
+      ) {
+        res.status(400).send("All input is required");
       }
-    );
-    user.token = token;
-    res.status(201).json(user);
-  } catch (err) {
-    console.log(err);
+      const oldUser = await userModel.UserModel.findOne({ UserMail });
+      if (oldUser) {
+        return res.status(409).send("User Already Exist. Please Login");
+      }
+
+      encryptedPassword = await (UserPassword && bcrypt.hash(UserPassword, 10));
+      const user = await userModel.UserModel.create({
+        UserName,
+        UserTel: parseInt(UserTel),
+        UserMail: UserMail.toLowerCase(),
+        UserPassword: encryptedPassword,
+        UserLogin,
+        UserType,
+        UserCountry
+      });
+      const token = jwt.sign(
+        { UserID: user._id, UserMail },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h"
+        }
+      );
+      user.token = token;
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
